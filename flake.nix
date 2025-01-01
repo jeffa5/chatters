71352b2f4a0aea12cd73bb76cec947551a3e4e57
Andrew Jeffery <dev@jeffas.net>
2025-01-01 18:48:42 +0900

    Initial setup

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=master";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        pkgs.rustc
+        pkgs.cargo
+        pkgs.rustfmt
+      ];
+    };
+  };
+}
