2cfaaee1831def93b4532b5bfa63fc4d898e2717
Andrew Jeffery <dev@jeffas.net>
2025-01-26 23:23:11 +1100

    Move main to main-signal and separate logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod backend_actor;
 pub mod backends;
+pub mod log;
 pub mod commands;
 pub mod keybinds;
 pub mod message;
---
+++ src/log.rs
@@ -0,0 +1,32 @@
+use std::{fs::{create_dir_all, File}, path::Path};
+
+pub struct LogTarget {
+    file: File,
+}
+
+impl LogTarget {
+    pub fn new<P: AsRef<Path>>(path: P) -> Self {
+        let path = path.as_ref();
+        create_dir_all(path.parent().unwrap()).unwrap();
+        Self {
+            file: File::create(path).unwrap(),
+        }
+    }
+}
+
+impl std::io::Write for LogTarget {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.file.write(buf)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.file.flush()
+    }
+}
+
+pub fn init_logger<P: AsRef<Path>>(path: P) {
+    let log_target = LogTarget::new(path);
+    env_logger::builder()
+        .target(env_logger::Target::Pipe(Box::new(log_target)))
+        .init();
+}
