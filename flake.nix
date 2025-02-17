c613824cc3b728de1f895d748b5162a2b21ad540
Andrew Jeffery <dev@jeffas.net>
2025-02-17 20:32:30 +1000

    Start working on nix builds

--- flake.nix
+++ flake.nix
@@ -9,7 +9,62 @@
   }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    customBuildRustCrateForPkgs = pkgs:
+      pkgs.buildRustCrate.override {
+        defaultCrateOverrides =
+          pkgs.defaultCrateOverrides
+          // {
+            libsignal-protocol = attrs: {
+              buildInputs = [pkgs.protobuf];
+            };
+            libsignal-service = attrs: {
+              buildInputs = [pkgs.protobuf];
+            };
+            presage-store-sled = attrs: {
+              buildInputs = [pkgs.protobuf];
+            };
+            chatters-signal = attrs: {
+              buildInputs = [
+                pkgs.pkg-config
+                pkgs.librandombytes
+                pkgs.openssl
+              ];
+            };
+            chatters-lib = attrs: {
+              buildInputs = [
+                pkgs.pkg-config
+                pkgs.librandombytes
+                pkgs.openssl
+              ];
+            };
+            pqcrypto-kyber = attrs: {
+              buildInputs = [
+                pkgs.pkg-config
+                pkgs.librandombytes
+                pkgs.openssl
+              ];
+            };
+          };
+      };
+    cargoNix = pkgs.callPackage ./Cargo.nix {
+      release = false;
+      buildRustCrateForPkgs = customBuildRustCrateForPkgs;
+    };
   in {
+    packages.${system} = rec {
+      chatters-local = cargoNix.workspaceMembers.chatters-local.build;
+      chatters-signal = cargoNix.workspaceMembers.chatters-signal.build;
+      chatters-matrix = cargoNix.workspaceMembers.chatters-matrix.build;
+      chatters = pkgs.symlinkJoin {
+        name = "chatters";
+        paths = [
+          chatters-local
+          chatters-signal
+          chatters-matrix
+        ];
+      };
+    };
+
     devShells.${system}.default = pkgs.mkShell {
       packages = [
         pkgs.rustc
@@ -17,6 +72,8 @@
         pkgs.rustfmt
         pkgs.clippy
         pkgs.cargo-insta
+
+        pkgs.crate2nix
 
         pkgs.openssl
         pkgs.pkg-config
